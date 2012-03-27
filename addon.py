dfb3f3f3155f6aa417b5b0539034a34c2fda6a01
sphere <sphere@dersphere.de>
2012-03-27 13:59:29 +0200

    fix missing comma

--- addon.py
+++ addon.py
@@ -112,7 +112,7 @@ class LogUploader(object):
                                            'Library/Logs/xbmc.log')):
                 # we are on OSX or ATV1
                 platform = 'OSX'
-                log_path = os.path.join(os.path.expanduser('~')
+                log_path = os.path.join(os.path.expanduser('~'),
                                         'Library/Logs')
             else:
                 # we are on IOS
